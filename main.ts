2c71ef40e538b733d73875d93632493ebe34aec8
perronnnn <116099717+perronnnn@users.noreply.github.com>
2022-10-31 13:01:09 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+let angle = 0
+servos.P0.setRange(10, 170)
+basic.forever(function () {
+    for (let index = 0; index <= 16; index++) {
+        angle = index * 10 + 10
+        servos.P0.setAngle(angle)
+        basic.pause(100)
+        break;
+    }
+    for (let index = 0; index <= 32; index++) {
+        angle = index * 5 + 10
+        servos.P0.setAngle(angle)
+        basic.pause(100)
+    }
+})
